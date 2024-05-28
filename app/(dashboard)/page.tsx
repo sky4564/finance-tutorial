1886b64129ced09aa06d9218527ae055c63f7670
김병민 <kbm5553@gnifrix.com>
2024-05-28 15:15:09 +0900

    clerk 적용  , sign in page css 작업

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return (
+        <p>
+            This is authenticated route
+        </p>
+    );
+};
+
+export default TestPage
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return (
+        <div>
+            test Page
+        </div>
+    );
+};
+
+export default TestPage
