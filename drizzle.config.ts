37745535b930fd645c6f9017fdf230213188003a
kbm5553 <kbm5553@gnifrix.com>
2024-05-31 01:11:17 +0900

    drizzle-kit 업데이트에 따른 config 변경 docs 기준으로 내용 수정

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,12 +5,24 @@ import { defineConfig } from 'drizzle-kit';
 config({ path: '.env.local'})
 
 export default defineConfig ({
-    schema: "./db/schema.ts",
-    dialect:"'postgresql'",
-    
+    schema: "./db/schema.ts",    
+    dialect:"postgresql",    
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL!,
     },
     verbose: true,
     strict: true
 });
+
+
+
+// import { defineConfig } from "drizzle-kit";
+ 
+// export default defineConfig({
+//   schema: "./schema/*",
+//   out: "./drizzle",
+//   dialect: 'postgresql',
+//   dbCredentials: {
+//     url: process.env.DB_URL,
+//   }
+// });
