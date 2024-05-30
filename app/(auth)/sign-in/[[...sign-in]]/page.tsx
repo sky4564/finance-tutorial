08d7273b860162fc68baa5db1e23ccf60d4be82e
김병민 <kbm5553@gnifrix.com>
2024-05-30 22:37:22 +0900

    db setting

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <div className="text-center space-y-4">
           <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
           <p className="text-base text-[#7E8CA0]">Log in or Create account to get back to your dashboard ! </p>
-        </div>6
+        </div>
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
             <SignIn path="/sign-in" />
---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env.local'})
+
+export default defineConfig ({
+    schema: "./db/schema.ts",
+    dialect:"'postgresql'",
+    
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL!,
+    },
+    verbose: true,
+    strict: true
+});
