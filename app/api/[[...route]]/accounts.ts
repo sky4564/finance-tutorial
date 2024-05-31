773dc67339444de058b8a707329cfba7453c1a92
김병민 <kbm5553@gnifrix.com>
2024-05-31 19:59:02 +0900

    hono 에서 auth 즉 로그인여부에 따라 비로그인시 error 처리 clerkMiddlewawre getAuth 사용

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,18 +1,42 @@
 import { Hono } from "hono";
+import { eq } from "drizzle-orm";
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { HTTPException } from "hono/http-exception"
+
 import { db } from "@/db/drizzle"
 import { accounts } from "@/db/schema"
 
-const app = new Hono() 
-    .get("/", async (c) => {
-    const data = await db
-        .select({
-            id: accounts.id,
-            name: accounts.name,
+
+const app = new Hono()
+    .get(
+        "/",
+        clerkMiddleware(),
+        async (c) => {
+            const auth = getAuth(c);
+
+            if (!auth?.userId) {
+                //이건 간단하게 쓸수있어서 좋음
+                //이거 써도 리턴타입에 안잡히게끔 추후 업데이트 예정인데 아직안된듯
+                //이거 쓰다가 문제생기면 밑에 error 처리로 복귀하기
+                return c.json({ error: "Unauthorized" } , 401)
+                
+                // 이걸쓰면 리턴 타입에 error 타입은 안잡혀서 깔끔함
+                
+                // throw new HTTPException(401, {
+                //     res: c.json({ error: "Unauthorze man" })
+                // })
+            }
+
+
+            const data = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name,
+                })
+                .from(accounts)
+                .where(eq(accounts.userId, auth.userId))
+
+            return c.json({ data })
         })
-        .from(accounts)
-
-
-    return c.json({ data })
-})
 
 export default app
