c4e14a399330f5295e217fbcfef56aa6d4e200d8
김병민 <kbm5553@gnifrix.com>
2024-05-31 19:56:57 +0900

    react query 파일 이용한 dashboard 작업 isLoading , accounts data 접근 깔짝 해둔거 다시 지우기

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,25 +5,10 @@ import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 
 export default function Home() {
-    const { data: accounts, isLoading } = useGetAccounts();
-
-    if(isLoading){
-        return (
-            <div>
-                Loading...
-            </div>
-        )
-    }
-
     return (
         <div>
-            {accounts?.map((account) => (
-                <div key={account.id}>
-                    {account.name}
-                </div>
-            ))}
+            Home Page
         </div>
-    );
-
+    )
 };
 
