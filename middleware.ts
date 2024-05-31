a0b72b7eb8f8f20016ef27f8681766512dc427ac
김병민 <kbm5553@gnifrix.com>
2024-05-31 19:54:10 +0900

    error handleing 보기위해서 middleware 에서 /api/ 접근 리다이렉트 안하게끔 설정

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher([
   '/',
   // api 비로그인시 완전차단
-  '/api(.*)'
+  // '/api(.*)'
 ]);
 
 export default clerkMiddleware((auth, request) => {
