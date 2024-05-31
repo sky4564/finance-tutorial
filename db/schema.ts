905f18ca74c3c2f5312cf9d8afc54741da61502e
김병민 <kbm5553@gnifrix.com>
2024-05-31 18:58:08 +0900

    drizzle setting 누락 채우기 , schema add

--- db/schema.ts
+++ db/schema.ts
@@ -3,7 +3,7 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable('accounts', {
     id: text('id').primaryKey(),    
-    name: text('name').notNull(),
-    power: text('power').notNull(),
+    plaidId: text("plaid_id"),
+    name: text('name').notNull(),    
     userId: text('user_id').notNull(),
 })
