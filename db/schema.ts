d897bd73c805ca1aa33216232f8141a374a4bfb7
김병민 <kbm5553@gnifrix.com>
2024-05-31 13:00:33 +0900

    drizzle-kit version 21 에 맞춰서 db setting

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,8 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable('accounts', {
-    id: text('id').primaryKey(),
+    id: text('id').primaryKey(),    
     name: text('name').notNull(),
+    power: text('power').notNull(),
     userId: text('user_id').notNull(),
 })
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,10 @@ config({ path: '.env.local'})
 
 export default defineConfig ({
     schema: "./db/schema.ts",
-    dialect:"'postgresql'",
+    dialect:"postgresql",
     
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL!,
     },
     verbose: true,
     strict: true
