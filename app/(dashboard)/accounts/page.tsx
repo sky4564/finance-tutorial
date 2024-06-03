748608faf13e5eab7f2113875de74dc9663949e9
sky4564 <qw486512@gmail.com>
2024-06-03 12:22:36 +0900

    delete 기능 구현전 db account 정보를 dashboard에 뛰워지게 해서 add new account 했을때 즉각적으로 보일수있도록

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,65 +1,69 @@
 "use client"
 
-import { Plus } from 'lucide-react';
+import { Loader2, Plus } from 'lucide-react';
 import { useNewAccount } from '@/features/accounts/hooks/use-new-accounts';
 import { DataTable } from '@/components/data.table';
+import { Skeleton } from '@/components/ui/skeleton';
 
 
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 
-import { Payment, columns } from '@/app/(dashboard)/accounts/columns';
-
-async function getData(): Promise<Payment[]> {
-    // Fetch data from your API here.
-    return [
-        {
-            id: "728ed52f",
-            amount: 100,
-            status: "pending",
-            email: "m@example.com",
-        },
-        // ...
-    ]
-}
-
-const data: Payment[] = [
-    {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-    },
-]
-
+import { columns } from './columns';
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
 
 const AccountsPage = () => {
-    const newAccount = useNewAccount();
+  const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || []
 
+  if (accountsQuery.isLoading) {
     return (
-        <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
-            <Card className='border-none drop-shadow-sm'>
-                <CardHeader className='gap-y-2 lg:flwx-row lg:items-center'>
-                    <CardTitle className='text-xl line-clamp-1'>
-                        Accounts Page
-                    </CardTitle>
-                    <Button onClick={newAccount.onOpen} size="sm">
-                        <Plus className="size-4 mr-2" />
-                        Add new
-                    </Button>
-                </CardHeader>
-                <CardContent>
-                    <DataTable columns={columns} data={data} />
-                </CardContent>
-            </Card>
-        </div>
-    );
+      <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+        <Card className='border-none drop-shadow-sm'>
+          <CardHeader>
+            <Skeleton className='h-8 w-48' />
+          </CardHeader>
+          <CardContent>
+            <div className='h-[500px] w-full flex items-center justify-center'>
+              <Loader2 className='size-6 text-slate-300 animate-spin' />
+            </div>
+          </CardContent>
+        </Card>  
+      </div>
+    )
+  }
+
+  return (
+    <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+      <Card className='border-none drop-shadow-sm'>
+        <CardHeader className='gap-y-2 lg:flwx-row lg:items-center'>
+          <CardTitle className='text-xl line-clamp-1'>
+            Accounts Page
+          </CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            filterKey='name'
+            columns={columns}
+            data={accounts}
+            onDelete={() => { }}
+            disabled={false}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default AccountsPage
