c825d8e73a327c1dbaa16b454ff59c6578fd5449
sky4564 <qw486512@gmail.com>
2024-06-05 18:35:24 +0900

    drizzle 변경사항 적용

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,29 +1,14 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-import { defineConfig } from 'drizzle-kit';
+config({ path: ".env.local" });
 
-config({ path: '.env.local'})
-
-export default defineConfig ({
-    schema: "./db/schema.ts",
-    dialect:"postgresql",
-    
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
-    verbose: true,
-    strict: true
+export default defineConfig({
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
 });
-
-
-
-// import { defineConfig } from "drizzle-kit";
- 
-// export default defineConfig({
-//   schema: "./schema/*",
-//   out: "./drizzle",
-//   dialect: 'postgresql',
-//   dbCredentials: {
-//     url: process.env.DB_URL,
-//   }
-// });
