e5b462450e8e06fc14ea814de66a5f0b4c9587c9
sky4564 <qw486512@gmail.com>
2024-06-29 20:34:55 +0900

    dashboard Charts end

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { DataCharts } from "@/components/data-charts"
 import { DataGrid } from "@/components/data-grid"
 
 export default function DashboardPage() {
@@ -7,6 +8,7 @@ export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
+      <DataCharts />
     </div>
   )
 };
